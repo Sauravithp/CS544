9445ea6beb720ceb09191d8002e51a627d11fec5
Sauravi Thapa <sauravithapa@Sauravis-MacBook-Pro.local>
2022-05-03 14:20:39 -0500

    add two module for exercise

---
+++ Exercise11_2/src/main/java/com/example/exercise11_2/Exercise112Application.java
@@ -0,0 +1,13 @@
+package com.example.exercise11_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise112Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise112Application.class, args);
+    }
+
+}
