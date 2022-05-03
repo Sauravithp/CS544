cafef49ab9ce084086581c838b7e8eee7e6bbbfa
Sauravi Thapa <sauravithapa@Sauravis-MacBook-Pro.local>
2022-05-03 14:33:20 -0500

    complete part 1

--- Exercise11_1/src/main/java/com/example/exercise11_1/Exercise111Application.java
+++ Exercise11_1/src/main/java/com/example/exercise11_1/Exercise111Application.java
@@ -1,13 +1,18 @@
 package com.example.exercise11_1;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import com.example.exercise11_1.domain.Greeting;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Exercise111Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Exercise111Application.class, args);
+        ConfigurableApplicationContext configurableApplicationContext=new ClassPathXmlApplicationContext
+                ("springConfig.xml");
+
+        Greeting greetingService=configurableApplicationContext.getBean("greetingService",Greeting.class);
+        greetingService.sayHello();
     }
 
 }
---
+++ Exercise11_1/src/main/java/com/example/exercise11_1/domain/Greeting.java
@@ -0,0 +1,22 @@
+package com.example.exercise11_1.domain;
+
+public class Greeting {
+
+    private String greeting;
+
+    public Greeting(){
+
+    }
+
+    public Greeting(String greeting){
+        this.greeting=greeting;
+    }
+
+    public void sayHello(){
+        System.out.println(greeting);
+    }
+
+    public void setGreeting(String greeting){
+        this.greeting=greeting;
+    }
+}
