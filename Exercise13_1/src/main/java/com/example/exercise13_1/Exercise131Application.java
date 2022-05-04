52c700edd8a7e384451e34268217dc7374ec5ff4
Sauravi Thapa <sauravithapa@Sauravis-MacBook-Pro.local>
2022-05-04 15:20:49 -0500

    add 13_1

---
+++ Exercise13_1/src/main/java/com/example/exercise13_1/Exercise131Application.java
@@ -0,0 +1,13 @@
+package com.example.exercise13_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise131Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise131Application.class, args);
+    }
+
+}
