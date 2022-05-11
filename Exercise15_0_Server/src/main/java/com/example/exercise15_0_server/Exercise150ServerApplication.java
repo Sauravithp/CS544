b4c29054519af034f0e233d96f74b03563c9d147
Sauravi Thapa <sauravithapa@Sauravis-MacBook-Pro.local>
2022-05-11 15:54:31 -0500

    complete

--- Exercise15_0_Server/src/main/java/com/example/exercise15_0_server/Exercise150ServerApplication.java
+++ Exercise15_0_Server/src/main/java/com/example/exercise15_0_server/Exercise150ServerApplication.java
@@ -2,10 +2,12 @@ package com.example.exercise15_0_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableCircuitBreaker
 public class Exercise150ServerApplication {
 
     public static void main(String[] args) {
--- Exercise15_0_Server/src/main/java/com/example/exercise15_0_server/controller/CountryController.java
+++ Exercise15_0_Server/src/main/java/com/example/exercise15_0_server/controller/CountryController.java
@@ -2,9 +2,11 @@ package com.example.exercise15_0_server.controller;
 
 import com.example.exercise15_0_server.domain.Country;
 import com.example.exercise15_0_server.feign.CountryFeign;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -14,9 +16,15 @@ public class CountryController {
     private CountryFeign countryService;
 
     @GetMapping("/countries")
+    @HystrixCommand(fallbackMethod = "readFallBack")
     public List<Country> read(){
        return countryService.read();
     }
+
+    public List<Country> readFallBack(){
+        return Arrays.asList(new Country());
+    }
+
 
 
 
